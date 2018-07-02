c572364af683a3bb6c6acc0e51f9b949f51ff635
chysome <onuohaeze@gmail.com>
2018-07-01 23:17:08 -0400

    Update HelloWorld.java

--- app-java/src/main/java/example/HelloWorld.java
+++ app-java/src/main/java/example/HelloWorld.java
@@ -1,22 +1,17 @@
 package example;
 
-public class HelloWorld() {
+public class HelloWorld {
 
   public void coveredByUnitTest() {
     System.out.println("coveredByUnitTest1");
     System.out.println("coveredByUnitTest2");
-    System.out.println("coveredByIntegrationTest1");
+    
   }
 
   public void coveredByIntegrationTest() {
     System.out.println("coveredByIntegrationTest1");
     System.out.println("coveredByIntegrationTest1");
-    System.out.println("coveredByIntegrationTest1");
-    System.out.println("coveredByIntegrationTest1");
-    System.out.println("coveredByIntegrationTest1");
-   
     
- 
   }
 
   public void notCovered() {
